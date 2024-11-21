e053c9ddaf8071bb276f44e883540aab6de69e3b
Supasek Laobutsa <laobutsasupasek@gmail.com>
2024-11-21 18:52:41 +0700

    Edit CardWorks

--- src/app/CardWorks/[id]/page.js
+++ src/app/CardWorks/[id]/page.js
@@ -1,26 +1,32 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import Image from "next/image";
+import Play from "@/app/assets/play.svg";
+import Pause from "@/app/assets/pause.svg";
 
-// Modify the getData function to load the entire JSON and filter by ID
 const getData = async () => {
-  const res = await fetch("/data/sourceProject.json"); // Fetch the entire JSON file
+  const res = await fetch("/data/sourceProject.json");
 
   if (!res.ok) {
     throw new Error("Failed to fetch product data");
   }
 
-  return await res.json(); // Return the entire product list
+  return await res.json();
 };
 
-const ProductPage = ({ params }) => {
-  const [product, setProduct] = useState(null);
+const ProjectPage = ({ params }) => {
+  const [project, setProject] = useState(null);
   const [loading, setLoading] = useState(true);
   const [id, setId] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showControls, setShowControls] = useState(false);
+
+  const videoRef = useRef(null);
 
   useEffect(() => {
     const resolveParams = async () => {
-      const { id: resolvedId } = await params; // Extract the id from params
-      setId(resolvedId); // Set the id state
+      const { id: resolvedId } = await params;
+      setId(resolvedId);
     };
 
     resolveParams();
@@ -28,112 +34,112 @@ const ProductPage = ({ params }) => {
 
   useEffect(() => {
     if (id) {
-      const fetchProductData = async () => {
+      const fetchProjectData = async () => {
         try {
-          const data = await getData(); // Get all products from the local JSON file
-          const product = data.find((item) => item.id === parseInt(id)); // Find product by ID
-          setProduct(product);
+          const data = await getData();
+          const project = data.find((item) => item.id === parseInt(id));
+          setProject(project);
         } catch (error) {
           console.error("Error fetching product:", error);
-          setProduct(null); // In case of error, set product to null
+          setProject(null);
         } finally {
           setLoading(false);
         }
       };
 
-      fetchProductData();
+      fetchProjectData();
     }
   }, [id]);
 
-  if (loading) return <div>Loading...</div>; // Show loading state while fetching
-  if (!product) return <div>Product not found</div>; // Show message if product is not found
+  const togglePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+        videoRef.current.muted = false;
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  if (loading) return <div>Loading...</div>;
+  if (!project) return <div>Project not found</div>;
+
+  const isImageFallback = (id >= 8 && id <= 33) || (id >= 35 && id <= 47);
 
   return (
     <div>
       <div className="relative">
-        <img
-          src={product.banner}
-          alt={product.title.title1}
-          className="w-full h-screen object-cover bg-center"
-        />
+        <div
+          className="w-full h-screen bg-center bg-fixed"
+          style={{
+            backgroundImage: `url(${project.banner})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
         <div className="absolute bottom-0 w-full h-24 bg-gradient-to-t from-[#040404] to-transparent" />
       </div>
 
-      <div className="md:hidden">
-        <div className="flex-col py-20 w-11/12 mx-auto flex justify-between">
-          <div>
-            <h1 className="text-white font-semibold text-6xl">
-              {product.created}
-            </h1>
-            <h1 className="text-gray-400 pb-2 text-xl font-semibold">Created by</h1>
-          </div>
-          <div>
-            <div className="py-10 md:py-0">
-              <h1 className="text-white font-semibold text-6xl">
-                {product.price} <span>Baht</span>
-              </h1>
-              <h1 className="text-white font-semibold text-xl">Price</h1>
-            </div>
-            <div>
-              <h1 className="text-white font-semibold text-6xl">
-                {product.developed} <span>Month</span>
-              </h1>
-              <h1 className="text-white font-semibold text-xl">Development</h1>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="md:block hidden">
-        <div className="py-20 w-11/12 mx-auto flex justify-between">
-          <div>
-            <h1 className="text-white font-semibold text-4xl xl:text-6xl">
-              {product.created}
-            </h1>
-            <h1 className="text-gray-400 font-semibold pb-2 text-xl">Created by</h1>
-          </div>
-          <div>
-            <h1 className="text-white font-semibold text-4xl xl:text-6xl">
-              {product.price} <span>Baht</span>
-            </h1>
-            <h1 className="text-white font-semibold text-xl">Price</h1>
-          </div>
-          <div>
-            <h1 className="text-white font-semibold text-4xl xl:text-6xl">
-              {product.developed}<span> Month</span>
-            </h1>
-            <h1 className="text-white font-semibold text-xl">Development</h1>
-          </div>
-        </div>
-      </div>
-
-
       <div className="w-11/12 mx-auto">
         <div className="w-9/12">
-          <h1 className="font-bold text-7xl text-white">
-            {product.header.header1}
+          <h1 className="font-bold text-7xl py-10 text-white">
+            {project.header.header1}
           </h1>
-          <p className="py-10 text-xl text-white">{product.title.title1}</p>
+          <p className="pb-10 text-xl text-white">{project.title.title1}</p>
         </div>
 
         <div>
-          <video
-            className="w-11/12 mx-auto h-auto border-2 rounded-lg" // Tailwind classes for styling
-            controls
-            autoPlay
-            loop
-          >
-            <source src={product.video} type="video/mp4" />
-          </video>
+          {isImageFallback ? (
+            <img
+              className="w-11/12 mx-auto h-auto rounded-lg"
+              src={project.banner}
+              alt={project.title.title1}
+            />
+          ) : (
+            <div
+              className="relative"
+              onMouseEnter={() => setShowControls(true)}
+              onMouseLeave={() => setShowControls(false)}
+            >
+              <div>
+                <video
+                  ref={videoRef}
+                  className="w-11/12 mx-auto h-auto rounded-lg"
+                  loop
+                  muted
+                  autoPlay
+                >
+                  <source src={project.video} type="video/mp4" />
+                </video>
+              </div>
+              {showControls && (
+                <div className="absolute flex justify-center items-center h-full w-full top-0">
+                  <button
+                    className="bg-white opacity-70 p-10 rounded-full"
+                    onClick={togglePlayPause}
+                  >
+                    <Image
+                      className="w-12"
+                      src={isPlaying ? Pause : Play}
+                      alt={isPlaying ? "Pause" : "Play"}
+                    />
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
         </div>
 
         <div className="w-9/12">
-          <h1 className="pt-10 font-bold text-7xl text-white">{product.header.header2}</h1>
-          <p className="py-10 text-xl text-white">{product.title.title2}</p>
+          <h1 className="pt-10 font-bold text-7xl text-white">
+            {project.header.header2}
+          </h1>
+          <p className="py-10 text-xl text-white">{project.title.title2}</p>
         </div>
       </div>
     </div>
   );
 };
 
-export default ProductPage;
+export default ProjectPage;
