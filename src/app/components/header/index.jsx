9b2a15910e2914157d5857ebd0b3303ce9c51759
Supasek Laobutsa <103197154+SupaseKLs@users.noreply.github.com>
2024-12-01 01:18:22 +0700

    Sucessfully

--- src/app/components/header/index.jsx
+++ src/app/components/header/index.jsx
@@ -1,13 +1,11 @@
 'use client';
 import styles from './style.module.scss';
-import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { motion, AnimatePresence, delay } from 'framer-motion';
 import { Slant as Hamburger } from 'hamburger-react';
 import Image from 'next/image';
 import Logo from '@/app/asset/img/logo.svg';
 import Nav from './nav';
-import { background } from './anim';
 
 
 
@@ -18,9 +16,6 @@ export default function Index() {
     const [isMenuOpen, setMenuOpen] = useState(false);
     const [isDropDown, setDropDown] = useState(false);
     const [isActive, setIsActive] = useState(false);
-
-    const Duray = 0.15;
-    const stagger = 0.025;
 
     // UseEffect for scroll position detection
     useEffect(() => {
@@ -176,54 +171,13 @@ export default function Index() {
                                             Work
                                         </motion.p>
                                     </li>
-                                    {['Our team', 'Review', 'FAQs', 'Contact', '|'].map((Menu) => (
+                                    {['Our team', 'Review', 'FAQs', 'Contact'].map((Menu) => (
                                         <motion.li
                                             initial="initial"
                                             whileHover="hovered"
                                             key={Menu}
                                             className="relative md:px-3 lg:px-6 cursor-pointer text-lg font-semibold overflow-hidden font-Inter "
                                         >
-                                            {/* <div>
-                                                <a href="">{Menu.split("").map((l, i) => (
-                                                    <motion.span
-                                                        variants={{
-                                                            initial: { y: 0 },
-                                                            hovered: { y: "-100%" }
-                                                        }}
-                                                        transition={{
-                                                            duration: Duray,
-                                                            ease: 'easeInOut',
-                                                            delay: stagger * i
-                                                        }}
-                                                        key={i}
-                                                        className='inline-block'
-                                                    >
-                                                        {l}
-                                                    </motion.span>
-                                                ))}</a>
-                                            </div>
-                                            <div className='absolute inset-0 md:px-3 lg:px-6'>
-                                                <a href="">{Menu.split("").map((l, i) => (
-                                                    <motion.span
-                                                        variants={{
-                                                            initial: { y: "100%" },
-                                                            hovered: { y: 0 }
-                                                        }}
-                                                        transition={{
-                                                            duration: Duray,
-                                                            ease: 'easeInOut',
-                                                            delay: stagger * i
-                                                        }}
-                                                        key={i}
-                                                        className='inline-block'
-                                                    >
-                                                        {l}
-                                                    </motion.span>
-                                                ))}</a>
-                                            </div> */}
-
-
-
                                             <motion.div
                                             className='text-text_color'
                                             variants = {{
@@ -244,25 +198,6 @@ export default function Index() {
                                             </motion.div>
                                         </motion.li>
                                     ))}
-
-                                    <li className="md:px-3 lg:px-6 cursor-pointer">
-                                        <a href="Contact">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                fill="none"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth={1.5}
-                                                stroke="currentColor"
-                                                className="size-6"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                                                />
-                                            </svg>
-                                        </a>
-                                    </li>
                                 </ul>
 
                                 {/* Mobile Menu */}
@@ -377,10 +312,6 @@ export default function Index() {
                                                                     </motion.li>
                                                                     <motion.li
                                                                         variants={items}
-                                                                        // initial={{ opacity: 0, x: -50 }}
-                                                                        // animate={{ opacity: 1, x: 0 }}
-                                                                        // exit={{ opacity: 0, x: -50 }}
-                                                                        // transition={{ delay: 0.4, duration: 0.3 }}
                                                                         className="py-4"
                                                                     >
                                                                         <motion.a
@@ -420,14 +351,6 @@ export default function Index() {
                                                     exit={{ opacity: 0 }}
                                                     transition={{ ease: 'easeInOut', delay: .2 }}
                                                     className="px-6 py-3 cursor-pointer">
-
-                                                    <motion.a href="Contact"
-                                                        whileHover={{ color: '#767676' }}
-                                                    >
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
-                                                        </svg>
-                                                    </motion.a>
                                                 </motion.li>
                                             </ul>
                                         </motion.div>
