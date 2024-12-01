69e27928368b262568952279c68f08801017b962
Supasek Laobutsa <103197154+SupaseKLs@users.noreply.github.com>
2024-12-01 16:00:59 +0700

    edit file json

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import Teams from './Our_team/Teams'
+import HomePage from '../app/Home/page'
 
 export default function Home() {
   return (
     <>
-    <Teams/>
+    <HomePage/>
     </>
   );
 }
